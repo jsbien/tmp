1aff2e98282b4bfa796565d42d859c827e4cbeb9
Janusz S. Bień <jsbien@mimuw.edu.pl>
2025-01-19 15:22:59 +0100

    substantial corrections

--- masks_deskew10.sh
+++ masks_deskew10.sh
@@ -1,3 +1,4 @@
 for file in *.png; do
-    convert "$file" -deskew 10% -despeckle  -debug Transform "$fil_Re"
+    convert "$file" +profile "*" -deskew 10% -type Bilevel -debug Transform "${file%.png}_R.png"
 done
+
